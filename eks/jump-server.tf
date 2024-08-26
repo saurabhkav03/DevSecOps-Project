1d074ebbdcd479e6b43ffb2f44631d510dbf37f1
saurabhkav03 <kaverisaurabh1998@gmail.com>
2024-08-26 22:05:24 +0530

    updated eks cluster file

---
+++ eks/jump-server.tf
@@ -0,0 +1,92 @@
+resource "aws_instance" "jump_server" {
+  ami                    = "ami-xxxxxx"  # Replace with your desired AMI ID (e.g., Amazon Linux 2)
+  instance_type          = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.jump_server_sg.id]
+  subnet_id              = module.vpc.public_subnets[0]  # Use the first public subnet
+
+  root_block_device {
+    volume_size = 30  # 30 GB storage
+  }
+
+  iam_instance_profile = aws_iam_instance_profile.admin_access.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y aws-cli
+              yum install -y jq
+              curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+              chmod +x ./kubectl
+              mv ./kubectl /usr/local/bin/kubectl
+              
+              # Install Helm
+              curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+              
+              # Install eksctl
+              curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+              mv /tmp/eksctl /usr/local/bin
+              EOF
+
+  tags = {
+    Name = "Jump Server"
+  }
+}
+
+resource "aws_security_group" "jump_server_sg" {
+  name        = "jump-server-sg"
+  description = "Allow SSH and access to EKS API and worker nodes"
+  vpc_id      = module.vpc.vpc_id  # Correct reference for VPC ID
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access from all IPs
+    # Uncomment the below line and comment the above line when you want to restrict access.
+    # cidr_blocks = [var.allowed_ip_cidr]  # Restrict SSH access to specific IP(s)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jump Server SG"
+  }
+}
+
+resource "aws_iam_instance_profile" "admin_access" {
+  name = "jump-server-admin-access"
+  role = aws_iam_role.admin_access.name
+}
+
+resource "aws_iam_role" "admin_access" {
+  name = "jump-server-admin-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = {
+    Name = "Jump Server Admin Role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "admin_access_policy_attachment" {
+  role       = aws_iam_role.admin_access.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"  # Administrator access
+}
+
+output "jump_server_public_ip" {
+  value = aws_instance.jump_server.public_ip
+}
--- eks/security-groups.tf
+++ eks/security-groups.tf
@@ -10,12 +10,11 @@ resource "aws_security_group" "allow_all" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_all_ipv4" {
   security_group_id = aws_security_group.allow_all.id
-  cidr_ipv4       = var.ingress_cidr_ipv4
+  cidr_ipv4         = var.ingress_cidr_ipv4
   from_port         = 0
   ip_protocol       = "-1"
   to_port           = 0
 }
-
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_all.id
