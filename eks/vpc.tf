2c56f9597d8d3833de896dcd3f9319192c19dcde
saurabhkav03 <kaverisaurabh1998@gmail.com>
2024-08-27 17:47:17 +0530

    update vpc.tf

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -1,4 +1,3 @@
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -17,4 +16,44 @@ module "vpc" {
     Name = var.name
     Env  = var.env
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = module.vpc.internet_gateway_id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = module.vpc.nat_gateway_id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# Associate all public subnets with the public route table
+resource "aws_route_table_association" "public" {
+  count = length(module.vpc.public_subnets)
+  subnet_id      = module.vpc.public_subnets[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate all private subnets with the private route table
+resource "aws_route_table_association" "private" {
+  count = length(module.vpc.private_subnets)
+  subnet_id      = module.vpc.private_subnets[count.index]
+  route_table_id = aws_route_table.private.id
 }
